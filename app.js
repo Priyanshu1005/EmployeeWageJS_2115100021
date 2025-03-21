00d29e3557889b136bd82ed2e43e53c63a971cde
priyanshu <priyanshu.sharma_cs.ccv21@gla.ac.in>
2025-03-21 16:10:53 +0530

    [Priyanshu Sharma] Add:UC3 implemented

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
-//UC 1
-const IS_Absent = 0;
-
-let empCheck = Math.floor(Math.random()* 10) % 2;
-
-if(empCheck == IS_Absent){
-    console.log("Employee is Absent");
-    return;
-}
-else{
-    console.log("Employee is Present");
+// UC1
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+  console.log("Employee is Absent");
+  return;
+} else {
+  console.log("Employee is PRESENT");
 }
 
-//UC2 Calculating the Employee Wage 
+// UC2
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 4;
+// let empHrs = 0;
+// empCheck = Math.floor(Math.random() * 10) % 3;
+// switch (empCheck) {
+//   case IS_PART_TIME:
+//     empHrs = PART_TIME_HOURS;
+//     break;
+//   case IS_FULL_TIME:
+//     empHrs = FULL_TIME_HOURS;
+//     break;
+//   default:
+//     empHrs = 0;
+// }
+// let empWage = empHrs * WAGE_PER_HOUR;
+// console.log("Emp Wage: " + empWage);
 
-empCheck = Math.floor(Math.random()* 10) % 3;
-
-switch (empCheck) {
+// UC 3
+function getWorkingHours(empCheck) {
+  switch (empCheck) {
     case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
+      return PART_TIME_HOURS;
     case IS_FULL_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
+      return FULL_TIME_HOURS;
     default:
-        empHrs = 4;
+      return 0;
+  }
 }
-let empWage = empHrs * WAGE_PER_HOUR ;
-console.log("Emp Wage: "  +empWage)
+
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
